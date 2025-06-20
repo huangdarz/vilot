8e547d5c84bb8e5d9d41a0237bc2c8e4f813b8dd
Darin Huang <huangdarin465@gmail.com>
2025-06-21 00:24:14 +1000

    Add rotation device

---
+++ include/vilot/rotation.h
@@ -0,0 +1,59 @@
+#pragma once
+#include "pros/rotation.hpp"
+#include "pros/rtos.hpp"
+#include "units.h"
+#include "vilot/filter.h"
+#include <utility>
+
+namespace vilot {
+
+class Encoder {
+  using degree_t = units::angle::degree_t;
+  using degrees_per_second_t = units::angular_velocity::degrees_per_second_t;
+
+public:
+  template <typename... Args>
+  Encoder(Args... args)
+      : rotation(std::forward<Args>(args)...), pos_filter(100, 50),
+        vel_filter(100, 50) {}
+
+  std::pair<degree_t, degrees_per_second_t> update();
+  void reset();
+
+  pros::Rotation rotation;
+
+private:
+  BiquadLowPassFilter pos_filter;
+  BiquadLowPassFilter vel_filter;
+};
+
+} // namespace vilot
+
+namespace vilot::device {
+
+class Rotation {
+  using degree_t = units::angle::degree_t;
+  using degrees_per_second_t = units::angular_velocity::degrees_per_second_t;
+
+public:
+  template <typename... Args>
+  Rotation(int8_t port)
+      : encoder(port), task(pros::Task::current()), position(0), velocity(0) {
+    this->task = pros::Task([this]() { this->update(); });
+  }
+
+  void start();
+
+  degree_t get_position();
+  degrees_per_second_t get_velocity();
+
+private:
+  void update();
+
+  Encoder encoder;
+  pros::Task task;
+  pros::MutexVar<degree_t> position;
+  pros::MutexVar<degrees_per_second_t> velocity;
+};
+
+} // namespace vilot::device
---
+++ src/vilot/rotation.cpp
@@ -0,0 +1,49 @@
+#include "vilot/rotation.h"
+#include "pros/rtos.h"
+#include "pros/rtos.hpp"
+#include <utility>
+
+namespace vilot {
+
+using namespace units::angle;
+using namespace units::angular_velocity;
+
+std::pair<degree_t, degrees_per_second_t> Encoder::update() {
+  int32_t pos = this->rotation.get_position();
+  int32_t vel = this->rotation.get_velocity();
+  auto new_pos = degree_t(this->pos_filter.apply((float)pos) / 100.0f);
+  auto new_vel =
+      degrees_per_second_t(this->vel_filter.apply((float)vel) / 100.0f);
+  return std::pair<degree_t, degrees_per_second_t>{new_pos, new_vel};
+}
+
+void Encoder::reset() {
+  this->pos_filter.reset();
+  this->vel_filter.reset();
+}
+
+} // namespace vilot
+
+namespace vilot::device {
+
+using namespace units::angle;
+using namespace units::angular_velocity;
+
+void Rotation::start() { this->task.notify(); }
+
+void Rotation::update() {
+  this->task.notify_take(true, TIMEOUT_MAX);
+  auto start = pros::millis();
+  for (;;) {
+    auto [pos, vel] = this->encoder.update();
+    *this->position.lock() = pos;
+    *this->velocity.lock() = vel;
+    pros::Task::delay_until(&start, 10);
+  }
+}
+
+degree_t Rotation::get_position() { return *this->position.lock(); }
+
+degrees_per_second_t Rotation::get_velocity() { return *this->velocity.lock(); }
+
+} // namespace vilot::device
